61818646e48bddca4b73c8ffd7616769bef97c65
naruto-sys <gavriil.belov@yandex.ru>
2023-05-11 00:03:00 +0300

    StartMenu.fxml and ChooseDifficultyMenu.fxml added Scene switching between them and appropriate controllers added

---
+++ src/main/java/com/example/sapper/DifficultyChangeController.java
@@ -0,0 +1,23 @@
+package com.example.sapper;
+
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.input.MouseEvent;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
+public class DifficultyChangeController {
+    @FXML
+    public void BackToStartGameButtonClicked(MouseEvent mouseEvent) throws IOException {
+        System.out.println("clicked back");
+        Parent root = FXMLLoader.load(getClass().getResource("StartMenu.fxml"));
+        Stage stage = (Stage)((Node)mouseEvent.getSource()).getScene().getWindow();
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
+}
