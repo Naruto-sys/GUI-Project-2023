ae89869ed47bc886ca6a122755a6de98a6a60dfc
xolllst <adilya.study31@gmail.com>
2023-06-03 20:04:19 +0300

    adding a "Back" button on pages of all levels

--- src/main/java/com/example/sapper/EasyLevelController.java
+++ src/main/java/com/example/sapper/EasyLevelController.java
@@ -1,11 +1,17 @@
 package com.example.sapper;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -27,4 +33,12 @@ public class EasyLevelController {
         field.PaintLevel(LevelGridPane, Difficulty.EASY, field, LevelMap);
     }
 
+    public void BackToDifficultyChangeMenuButtonClicked(MouseEvent mouseEvent) throws IOException {
+        System.out.println("clicked back");
+        Parent root = FXMLLoader.load(getClass().getResource("ChooseDifficultyMenu.fxml"));
+        Stage stage = (Stage)((Node)mouseEvent.getSource()).getScene().getWindow();
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
+    }
 }
--- src/main/java/com/example/sapper/MediumLevelController.java
+++ src/main/java/com/example/sapper/MediumLevelController.java
@@ -1,9 +1,16 @@
 package com.example.sapper;
 
 import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
 import javafx.scene.control.Label;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.GridPane;
+import javafx.stage.Stage;
 
+import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -22,5 +29,14 @@ public class MediumLevelController {
     public void initialize() {
         LevelMap = field.CreateField(Difficulty.MEDIUM);
         field.PaintLevel(LevelGridPane, Difficulty.MEDIUM, field, LevelMap);
+    }
+
+    public void BackToDifficultyChangeMenuButtonClicked(MouseEvent mouseEvent) throws IOException {
+        System.out.println("clicked back");
+        Parent root = FXMLLoader.load(getClass().getResource("ChooseDifficultyMenu.fxml"));
+        Stage stage = (Stage)((Node)mouseEvent.getSource()).getScene().getWindow();
+        Scene scene = new Scene(root);
+        stage.setScene(scene);
+        stage.show();
     }
 }
